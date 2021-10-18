772b958797b9725f952a02843356484969b6f566
Gabriel Camargo <gcamargobittencourt@gmail.com>
2021-10-18 13:34:29 -0300

    aula de configuraçao webpack

--- react-fundamentos/build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var _user$adress;
-
-var user = {};
-console.log(user === null || user === void 0 ? void 0 : (_user$adress = user.adress) === null || _user$adress === void 0 ? void 0 : _user$adress.street);
--- react-fundamentos/src/index.js
+++ react-fundamentos/src/index.js
@@ -1,5 +1,9 @@
-const user = {
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-}
-
-console.log(user?.adress?.street)
+ReactDOM.render(
+  <span>
+  <h1 id="title">Hello, World!!!!</h1>
+  </span>,
+  document.getElementById('root')
+)
