ee8721bf076a343864bfd0319dbd63562343f019
Gabriel Camargo <gcamargobittencourt@gmail.com>
2021-10-18 12:42:21 -0300

    babel configurado

---
+++ react-fundamentos/build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var _user$adress;
+
+var user = {};
+console.log(user === null || user === void 0 ? void 0 : (_user$adress = user.adress) === null || _user$adress === void 0 ? void 0 : _user$adress.street);
