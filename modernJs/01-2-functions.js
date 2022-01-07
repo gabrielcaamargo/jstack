f59f0698fe41fc5ac739be0b795392e2cb6439e1
gaabrielcamargo <gcamargobittencourt@gmail.com>
2022-01-07 10:03:48 -0300

    Aula 01 - Com 1 arquivo contendo cada função (100% documentado no Notion)

---
+++ modernJs/01-2-functions.js
@@ -0,0 +1,6 @@
+function soma(primeiroParametro, ...argumentos){
+  console.log({primeiroParametro})
+  console.log({argumentos})
+}
+
+soma(1, 2, 3, 4, 5, 6, 'Gabriel')
---
+++ modernJs/01-3-functions.js
@@ -0,0 +1,8 @@
+const someNumber = 10
+const calculator = () => (
+  someNumber >= 10 
+  ? "Greater than 10" 
+  : "Lower than 10"
+  )
+
+console.log(calculator())
