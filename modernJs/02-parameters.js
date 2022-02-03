121eb5bff872fd7c7b92ed4f3c29e5eef7fa53ab
Gabriel Camargo <gcamargo.dev@gmail.com>
2022-02-03 20:12:49 -0300

    .

---
+++ modernJs/02-parameters.js
@@ -0,0 +1,7 @@
+function printGreetings(message = 'Mensagem padrão'){
+  console.log(message)
+}
+
+printGreetings('Seja bem-vindo')
+
+//We are declarating a default result to the argument. When the user, doesn't pass anything as the parameter, the default parameter will be used. And whe he pass, the new parameter will be used.
