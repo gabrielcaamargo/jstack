e15919db5a675a5ac30c0f3157b52e09dfc5e15b
Gabriel Camargo <gcamargo.dev@gmail.com>
2022-02-03 12:44:05 -0300

    arrow function uses / declarations

---
+++ modernJs/01-1-functions.js
@@ -0,0 +1,8 @@
+function soma(primeiroParametro, ...argumentos){
+  console.log({primeiroParametro})
+  console.log({argumentos})
+}
+
+soma(1, 2, 3, 4, 5, 6, 7, 'Gabriel')
+
+//Function keyword has its own arguments, same as this keyword. Arrow function gets its "father" arguments, same as this keyword. 
