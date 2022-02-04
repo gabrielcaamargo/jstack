30df3cf7610f17ef5fa93e055e566e0a8ceafbe3
Gabriel Camargo <gcamargo.dev@gmail.com>
2022-02-04 20:14:26 -0300

    template strings

---
+++ modernJs/Aula 06/06-templateString.js
@@ -0,0 +1,6 @@
+const nome = 'Gabriel'
+
+console.log(`Bem vindo ${Math.random() > 0.5 ? 'Sim' : 'Não'}`)
+//Using template strings we can break lines
+
+//We can use JS Expressions using template string
