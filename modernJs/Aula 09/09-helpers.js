32b30ab2e9cd6e19d9e096e44ee7d7b6d519066e
Gabriel Camargo <gcamargo.dev@gmail.com>
2022-02-06 15:11:42 -0300

    includes/startsWith/endsWith

---
+++ modernJs/Aula 09/09-helpers.js
@@ -0,0 +1,11 @@
+const tech = 'Node.js'
+const array = ['Nodejs', 'React','TypeScript']
+
+const includesString = tech.includes('Node')
+const includesArray = array.includes('React')
+
+const startsWith = tech.startsWith('No')
+const endsWith = tech.endsWith('js')
+console.log({endsWith})
+
+//ALL OF THIS METHODS ARE CASE SENSITIVE
