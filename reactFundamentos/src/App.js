0db06b749d57b28169567b973a223ed7f719f01b
gaabrielcamargo <gcamargo.dev@gmail.com>
2022-02-13 18:03:40 -0300

    what is props

--- reactFundamentos/src/App.js
+++ reactFundamentos/src/App.js
@@ -1,10 +1,38 @@
 import React from "react"
 
+import Post from './Post'
+
+const titulo01 = 'Título Zero UM'
+
 function App(){
   return (
     <>
-      <h1>Componente App</h1>
-      <h2>Subtítulo</h2>
+      <h1 id="idDoTitulo">JStack's Blog</h1>
+      <h2>Posts da semana</h2>
+    
+    <hr />
+
+    <Post 
+      post={{
+        title: 'Título da notícia 01',
+        subtitle: 'Subtítulo da notícia 01'
+      }}
+    />
+
+    <Post 
+      post={{
+        title: 'Título da notícia 02',
+        subtitle: 'Subtítulo da notícia 02'
+      }}
+    />
+
+    <Post 
+      post={{
+        title: 'Título da notícia 03',
+        subtitle: 'Subtítulo da notícia 03'
+      }}
+    />
+
     </>
     )
     
