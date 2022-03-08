3860031409104ac36c66ba456ef5a1096584f229
gaabrielcamargo <gcamargo.dev@gmail.com>
2022-03-08 06:42:06 -0300

    restarted course module

--- reactFundamentos/src/App.js
+++ reactFundamentos/src/App.js
@@ -1,51 +1,14 @@
-import React, { useState } from "react"
+import React from "react"
 
-import Post from './Post'
-import Header from './Header'
-
+const category = 'Posts da semana'
 
 function App(){
-
-  const [posts, setPosts] = useState([
-    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20, read: false },
-    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10, read: true },
-    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50, read: false },
-    { id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 50, read: true },
-  ])
-
-  function handleRefresh(){
-    setTimeout(()=>{
-      setPosts( prevState =>[
-        ...prevState,
-        {
-          id: Math.random(),
-          title: `Title#0${prevState.length + 1}`,
-          subtitle: `Title#0${prevState.length + 1}`,
-          likes: 50,  
-        }
-      ])
-    },500)
-  }
-
   return (
     <>
-    <Header title="Blog do JStack">
-      <h2>Posts da semana 
-        <button onClick={handleRefresh}>Atualizar</button>
-      </h2>
-    </Header>
-    
-    <hr /> 
-    
-    {posts.map(post => (
-      <Post 
-        key={post.id}
-        likes={post.likes}
-        post={post}
-      />
-    ))}
+      <h1>Jstack's Blog</h1>
+      <h2>{category}</h2>  
     </>
-    )
+  )
 }
 
 export default App
