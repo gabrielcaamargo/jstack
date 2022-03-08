61e4a916f4b10533b2f44a58289cf9c7c93ca2cc
gaabrielcamargo <gcamargo.dev@gmail.com>
2022-03-07 22:18:26 -0300

    conditional rendering

--- reactFundamentos/src/App.js
+++ reactFundamentos/src/App.js
@@ -7,22 +7,22 @@ import Header from './Header'
 function App(){
 
   const [posts, setPosts] = useState([
-    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20 },
-    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10 },
-    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50 },
-    { id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 50 },
+    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20, read: false },
+    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10, read: true },
+    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50, read: false },
+    { id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 50, read: true },
   ])
 
   function handleRefresh(){
     setTimeout(()=>{
-      setPosts((prevState)=>[
-          ...prevState, 
-          {
-            id: Math.random(),
-            title: `Title#0${prevState.length +1}`,
-            subtitle: `Sub#0${prevState.length +1}`,
-            likes: 50
-          }
+      setPosts( prevState =>[
+        ...prevState,
+        {
+          id: Math.random(),
+          title: `Title#0${prevState.length + 1}`,
+          subtitle: `Title#0${prevState.length + 1}`,
+          likes: 50,  
+        }
       ])
     },500)
   }
@@ -35,16 +35,13 @@ function App(){
       </h2>
     </Header>
     
-    <hr />
+    <hr /> 
     
     {posts.map(post => (
       <Post 
         key={post.id}
         likes={post.likes}
-        post={{
-          title: post.title,
-          subtitle: post.subtitle
-        }}
+        post={post}
       />
     ))}
     </>
--- reactFundamentos/src/Post.js
+++ reactFundamentos/src/Post.js
@@ -2,24 +2,29 @@ import React from "react";
 import propTypes from "prop-types";
 
 export default function Post (props) {
+
   return (
     <>
       <article>
-        <strong>{props.post.title}</strong> <br />
+        <strong>
+          {props.post.read && <s>{props.post.title}</s>}
+          {!props.post.read && props.post.title}
+        </strong> <br />
         <small>{props.post.subtitle}</small>
         <br />
-        Media: {props.likes / 2}
-      </article>
-      <br />
-  </>
+         Media: {props.post.likes / 2}
+        </article>
+        <br />
+    </>
   )
 }
 
 Post.propTypes = {
-  likes: propTypes.number.isRequired, 
   
   post: propTypes.shape({
     title: propTypes.string.isRequired,
-    subtitle: propTypes.string.isRequired
+    subtitle: propTypes.string.isRequired,
+    likes: propTypes.number.isRequired, 
+    read: propTypes.bool.isRequired, 
   }).isRequired,
 }
