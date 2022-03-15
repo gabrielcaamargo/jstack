17d2080c8aefc03524a69689944823a6a4c7c6d8
gaabrielcamargo <gcamargo.dev@gmail.com>
2022-03-15 06:26:08 -0300

    inline styles class

--- reactFundamentos/src/Header.js
+++ reactFundamentos/src/Header.js
@@ -7,11 +7,17 @@ import {ThemeContext} from './ThemeContext';
 export default function Header(props) {
   const { onToggleTheme } = useContext(ThemeContext)
   return(
-    <>
+    <div
+      style={{
+        background: '#ccc',
+        fontFamily: 'sans-serif',
+        marginBottom: '24px',
+      }}
+    >
       <h1>{props.title}</h1>
       <Button onClick={onToggleTheme}>Mudar tema</Button>
       {props.children}
-    </>
+    </div>
   )
 }
 
