9b05250f676282fdc4cc97e624be426f90018dcd
gaabrielcamargo <gcamargo.dev@gmail.com>
2022-03-15 06:13:18 -0300

    context api class

--- reactFundamentos/src/App.js
+++ reactFundamentos/src/App.js
@@ -1,13 +1,58 @@
-import React from "react"
+import React, { useState } from "react"
 
-const category = 'Posts da semana'
+import Header from "./Header"
+import Post from "./Post"
+
+import { ThemeProvider } from './ThemeContext'
 
 function App(){
+
+  const [posts, setPosts] = useState([
+    {id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20, read: false},
+    {id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10, read: true},
+    {id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50, read: false},
+    {id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 50, read: true}
+  ])
+
+
+  function handleRefresh(){
+    
+    setPosts((prevState)=> [
+        ...prevState,
+        {
+           id: Math.random(), 
+           title: `Title#0${prevState.length + 1}`, 
+           subtitle: `Sub#0${prevState.length + 1}`, 
+           likes: 50
+        }
+      ])
+  }
+
+  function handleRemovePost(postId){
+    setPosts(prevState => (
+      prevState.filter(post => post.id !== postId)
+    ))
+  }
+
   return (
-    <>
-      <h1>Jstack's Blog</h1>
-      <h2>{category}</h2>  
-    </>
+    <ThemeProvider> 
+      <Header>
+        <h2>Posts da semana
+        &nbsp;
+        <button onClick={handleRefresh}>Atualizar</button>
+        </h2>
+      </Header>
+
+      <hr />
+      
+      {posts.map(post => (
+        <Post 
+          key={post.id}
+          onRemove={handleRemovePost}
+          post={post}
+        />
+      ))}
+    </ThemeProvider>
   )
 }
 
