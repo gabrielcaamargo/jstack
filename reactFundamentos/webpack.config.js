9fe2647e06b8573c2fd29e126e3094fd50e684ed
gaabrielcamargo <gcamargo.dev@gmail.com>
2022-03-16 23:20:49 -0300

    introduction to theming inside styled components

--- reactFundamentos/webpack.config.js
+++ reactFundamentos/webpack.config.js
@@ -1,41 +1,27 @@
-const path = require('path')
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin =  require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle[hash].js'
-  },
+    filename: 'bundle[hash].js',
+  }, 
   plugins: [
-    new HtmlWebPackPlugin(
-      {
-        template: path.resolve(__dirname, 'public', 'index.html')
-      }
-    ),
-    new CleanWebpackPlugin()
+    new HtmlWebpackPlugin ({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+    new CleanWebpackPlugin(),
   ],
-
   module: {
-    rules : [
+    rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: 'babel-loader',
       },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader', 
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-            }
-          }
-        ]
-      },      
       {
         test: /\.scss$/,
         use: [
@@ -44,15 +30,15 @@ module.exports = {
             loader: 'css-loader',
             options: {
               modules: true,
-            }
+            },
           },
           'sass-loader',
-        ], 
-      }
-    ]
+        ],
+      },
+    ],
+    
   },
-
   devServer: {
     port: 3000,
   }
-}
+};
