26c48d95e691618078ef9153a6827710055e0785
gaabrielcamargo <gcamargo.dev@gmail.com>
2022-03-18 20:19:22 -0300

    setting the dark/light themes to components

--- reactFundamentos/src/components/Footer/styles.js
+++ reactFundamentos/src/components/Footer/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.footer`
-  background: #111;
+  background-color: ${({ theme }) => theme.footerBackgroundColor };
   height: 70px;
   display: flex;
   align-items: center;
--- reactFundamentos/src/styles/themes/light.js
+++ reactFundamentos/src/styles/themes/light.js
@@ -4,5 +4,9 @@ export default {
   ...commonStyles,
   backgroundColor: '#fff',
   textColor: '#222',
+  headerBackgroundColor: '#aaa',
+  footerBackgroundColor: '#aaa',
+  postBackgroundColor: '#ddd',
+
 };
 
