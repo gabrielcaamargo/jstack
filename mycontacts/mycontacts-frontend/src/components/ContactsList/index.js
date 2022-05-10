43bc544714cc774ead669b6fa98cc9a3cb2c203f
gaabrielcamargo <gcamargo.dev@gmail.com>
2022-05-09 23:26:33 -0300

    set routes.js & created pages folder & created ContactsList component

--- mycontacts/mycontacts-frontend/src/components/ContactsList/index.js
+++ mycontacts/mycontacts-frontend/src/components/ContactsList/index.js
@@ -1,4 +1,10 @@
-import { Container, Header } from './styles';
+import {
+  Container, Header, ListContainer, Card,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export default function ContactsList() {
   return (
@@ -7,6 +13,75 @@ export default function ContactsList() {
         <strong>3 contatos</strong>
         <a href="/">Novo contato</a>
       </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Gabriel Camargo</strong>
+              <small>instagram</small>
+            </div>
+            <span>gcamargo.dev@gmail.com</span>
+            <span>(51) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Gabriel Camargo</strong>
+              <small>instagram</small>
+            </div>
+            <span>gcamargo.dev@gmail.com</span>
+            <span>(51) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Gabriel Camargo</strong>
+              <small>instagram</small>
+            </div>
+            <span>gcamargo.dev@gmail.com</span>
+            <span>(51) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
     </Container>
   );
 }
